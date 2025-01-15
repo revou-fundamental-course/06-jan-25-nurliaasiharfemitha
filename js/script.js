3f400e7efd635d884454457a60448227c871a152
nurliaasiharfemitha <nurliamitha1522@gmail.com>
2025-01-15 22:23:13 +0700

    user greetings

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-// Ini File Javascript
+//user greeting
+function replaceName() {
+    let nama = prompt("Halo, siapakah nama anda?","");
+    document.getElementById("user-greeting").innerHTML = nama
+}
+
+replaceName()
+
+function formValidation (){
+   let nameInput = document.getElementById('nameinput');
+   console.log(nameInput);
+}
+
+document.getElementById('submit-button').addEventListener('click', formValidation);
+
